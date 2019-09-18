c4ab53a0b0ad9a185d18a6fd39ef1f1a9f9ef396
Noguera Arthur (HES) <arthur.nogueraa@hes.adhes.hesge.ch>
2019-09-18 18:18:32 +0200

    deuxieme sauvegarde

--- bonjour.py
+++ bonjour.py
@@ -1,5 +1,5 @@
 """ bienvenue"""
-donnöes : le semestre d'entrée
+données : le semestre d'entrée
 resultat : afficher un message de  bienvenue au etudiant-e
 
 ### declaration des variables
@@ -7,3 +7,5 @@ resultat : afficher un message de  bienvenue au etudiant-e
 ###séquence d' oprération
 print("bonjour!")
 print("bioenvenue aux etudiants-es 2019 2020!")
+print("c'est pour le TP d info")
+
